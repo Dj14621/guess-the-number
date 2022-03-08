34776f83092bccf5dd6e347794dbd49b1c306356
Dj <haradj.djelloul@gmail.com>
2022-03-08 14:12:57 -0500

    started Start game method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void (String []args) {
-        
+//Declare a Scanner object
+    private static final Scanner  scanner = new Scanner(System.in);
+
+        public static void main (String [] args) {
+            //start Game
+            startGame();
+
+            //Finish Game
+            System.out.println("\n Thank you for plaing!");
+    }
+
+    //Putting methods here
+    public static void startGame(){
+        System.out.println("Welcome...");
+            //Game Logic
+    }
+
+// getting user name
+    public static String getUserName() {
+        System.out.println("Please enter your name: ");
+        String name = scanner.next();
+        return "Welcome " + name + " to the guess the number game!";
     }
 }
